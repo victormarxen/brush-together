5f13211c831ece553299265a59e0f9ba9ebc61d1
Conor Kelly <conorkellymail@gmail.com>
2022-08-23 18:13:16 +0100

    Listings page - HTML is static

--- app/models/toothbrush.rb
+++ app/models/toothbrush.rb
@@ -1,4 +1,4 @@
 class Toothbrush < ApplicationRecord
-  belongs_to :announcer, class_name: 'User', foreign_key: 'announcer_id', required: true
+  belongs_to :announcer, class_name: 'User', foreign_key: 'user_id', required: true
   has_many_attached :photos
 end
