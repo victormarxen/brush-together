1965bd1ea5c21a916c672ac292a319a8e4a9b466
Victor Marxen <victor.marxen@hotmail.de>
2022-08-23 16:34:01 +0100

    Model Review Created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_141214) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_153107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_141214) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.bigint "toothbrush_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["toothbrush_id"], name: "index_reviews_on_toothbrush_id"
+  end
+
   create_table "toothbrushes", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -70,5 +79,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_141214) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "reviews", "toothbrushes"
   add_foreign_key "toothbrushes", "users"
 end
