41bf9b1211411c9558558b0805bc09a6b4ce0f0d
Andre Bakiewicz <bakiewicz@gmail.com>
2022-08-24 11:43:17 +0100

    authorizations applied

--- app/controllers/toothbrushes_controller.rb
+++ app/controllers/toothbrushes_controller.rb
@@ -1,21 +1,26 @@
 class ToothbrushesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_toothbrush, only: %i[show edit update destroy]
 
   def index
     @toothbrushes = Toothbrush.all
+    @toothbrushes = policy_scope(Toothbrush)
   end
 
   def show
     @review = Review.new
+    authorize @toothbrush
   end
 
   def new
     @toothbrush = Toothbrush.new
+    authorize @toothbrush
   end
 
   def create
     @toothbrush = Toothbrush.new(toothbrush_params)
     @toothbrush.announcer = current_user
+    authorize @toothbrush
     if @toothbrush.save
       redirect_to toothbrush_path(@toothbrush), notice: 'Toothbrush was successfully created.'
     else
@@ -24,14 +29,17 @@ class ToothbrushesController < ApplicationController
   end
 
   def edit
+    authorize @toothbrush
   end
 
   def update
+    authorize @toothbrush
     @toothbrush.update(toothbrush_params)
     redirect_to toothbrush_path(@toothbrush)
   end
 
   def destroy
+    authorize @toothbrush
     @toothbrush.destroy
     redirect_to toothbrushes_path, status: :see_other
   end
