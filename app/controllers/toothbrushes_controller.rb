938d0544c09e23f0eebf0ac7e0f06860dca134e9
Victor Marxen <victor.marxen@hotmail.de>
2022-08-25 14:42:46 +0100

    Map is now working in the index

--- app/controllers/toothbrushes_controller.rb
+++ app/controllers/toothbrushes_controller.rb
@@ -5,6 +5,14 @@ class ToothbrushesController < ApplicationController
   def index
     @toothbrushes = Toothbrush.all
     @toothbrushes = policy_scope(Toothbrush)
+    @markers = @toothbrushes.geocoded.map do |toothbrush|
+      {
+        lat: toothbrush.latitude,
+        lng: toothbrush.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { toothbrush: toothbrush }),
+        image_url: helpers.asset_url("brush.jpg")
+      }
+    end
   end
 
   def show
